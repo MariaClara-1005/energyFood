eb9a1170cdab766462b68f1fab9f19e3837c206c
Maria Clara <maria_clara@maria.clara>
2024-11-01 12:17:21 -0300

    link de acesso

--- at01/at01/settings.py
+++ at01/at01/settings.py
@@ -108,6 +108,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
